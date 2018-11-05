03887a167e08ed71192eb9319c63902d4f4e5a59
melikebektas <melikebektas95@gmail.com>
2018-11-05 14:17:55 +0200

    karar agaci

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace karar_agaci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbHava = new System.Windows.Forms.ComboBox();
+            this.cmbIsi = new System.Windows.Forms.ComboBox();
+            this.cmbNem = new System.Windows.Forms.ComboBox();
+            this.cmbRüzgar = new System.Windows.Forms.ComboBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.btnEntropi = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAgc = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(533, 334);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hava:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Isı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 449);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nem:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 476);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Rüzgar:";
+            // 
+            // cmbHava
+            // 
+            this.cmbHava.FormattingEnabled = true;
+            this.cmbHava.Items.AddRange(new object[] {
+            "Güneşli",
+            "Yağmurlu",
+            "Bulutlu"});
+            this.cmbHava.Location = new System.Drawing.Point(71, 391);
+            this.cmbHava.Name = "cmbHava";
+            this.cmbHava.Size = new System.Drawing.Size(121, 21);
+            this.cmbHava.TabIndex = 5;
+            // 
+            // cmbIsi
+            // 
+            this.cmbIsi.FormattingEnabled = true;
+            this.cmbIsi.Items.AddRange(new object[] {
+            "Sıcak",
+            "Ilık",
+            "Soğuk"});
+            this.cmbIsi.Location = new System.Drawing.Point(71, 419);
+            this.cmbIsi.Name = "cmbIsi";
+            this.cmbIsi.Size = new System.Drawing.Size(121, 21);
+            this.cmbIsi.TabIndex = 6;
+            // 
+            // cmbNem
+            // 
+            this.cmbNem.FormattingEnabled = true;
+            this.cmbNem.Items.AddRange(new object[] {
+            "Yüksek",
+            "Normal"});
+            this.cmbNem.Location = new System.Drawing.Point(71, 446);
+            this.cmbNem.Name = "cmbNem";
+            this.cmbNem.Size = new System.Drawing.Size(121, 21);
+            this.cmbNem.TabIndex = 7;
+            // 
+            // cmbRüzgar
+            // 
+            this.cmbRüzgar.FormattingEnabled = true;
+            this.cmbRüzgar.Items.AddRange(new object[] {
+            "Hafif",
+            "Kuvvetli"});
+            this.cmbRüzgar.Location = new System.Drawing.Point(71, 476);
+            this.cmbRüzgar.Name = "cmbRüzgar";
+            this.cmbRüzgar.Size = new System.Drawing.Size(121, 21);
+            this.cmbRüzgar.TabIndex = 8;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(6, 511);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(197, 41);
+            this.btnTest.TabIndex = 9;
+            this.btnTest.Text = "Test Et";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 574);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Oyun Sonucu:";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Location = new System.Drawing.Point(84, 571);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(119, 20);
+            this.txtSonuc.TabIndex = 11;
+            // 
+            // btnEntropi
+            // 
+            this.btnEntropi.Location = new System.Drawing.Point(300, 356);
+            this.btnEntropi.Name = "btnEntropi";
+            this.btnEntropi.Size = new System.Drawing.Size(196, 47);
+            this.btnEntropi.TabIndex = 12;
+            this.btnEntropi.Text = "Entropi Hesapla";
+            this.btnEntropi.UseVisualStyleBackColor = true;
+            this.btnEntropi.Click += new System.EventHandler(this.btnEntropi_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(241, 409);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 160);
+            this.listBox1.TabIndex = 13;
+            // 
+            // btnAgc
+            // 
+            this.btnAgc.Location = new System.Drawing.Point(300, 571);
+            this.btnAgc.Name = "btnAgc";
+            this.btnAgc.Size = new System.Drawing.Size(196, 42);
+            this.btnAgc.TabIndex = 14;
+            this.btnAgc.Text = "Karar Ağacı Çiz";
+            this.btnAgc.UseVisualStyleBackColor = true;
+            this.btnAgc.Click += new System.EventHandler(this.btnAgc_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(581, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(681, 647);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 733);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAgc);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnEntropi);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.cmbRüzgar);
+            this.Controls.Add(this.cmbNem);
+            this.Controls.Add(this.cmbIsi);
+            this.Controls.Add(this.cmbHava);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbHava;
+        private System.Windows.Forms.ComboBox cmbIsi;
+        private System.Windows.Forms.ComboBox cmbNem;
+        private System.Windows.Forms.ComboBox cmbRüzgar;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Button btnEntropi;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAgc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace karar_agaci
+{
+    
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 1700;
+            this.Height = 800;
+        }
+
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Blue);
+        private System.Drawing.Pen pen2 = new System.Drawing.Pen(Color.HotPink,3);
+        Brush myBrush = new SolidBrush(Color.Blue);
+
+
+        
+        Font drawFont = new Font("Arial", 16);
+        Font drawFont2 = new Font("Arial", 12);
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+            
+       
+        {
+            
+            DataTable tablo = new DataTable();
+            tablo.Columns.Add("Hava");
+            tablo.Columns.Add("Isı");
+            tablo.Columns.Add("Nem");
+            tablo.Columns.Add("Rüzgar");
+            tablo.Columns.Add("Oyun");
+            dataGridView1.DataSource = tablo;
+
+            tablo.Rows.Add("güneşli", "sıcak", "yüksek", "hafif", "hayir");
+            tablo.Rows.Add("güneşli", "sıcak", "yüksek", "kuvvetli", "hayir");
+            tablo.Rows.Add("bulutlu", "sıcak", "yüksek", "hafif", "evet");
+            tablo.Rows.Add("yağmurlu", "ılık", "yüksek", "hafif", "evet");
+            tablo.Rows.Add("yağmurlu", "soğuk", "normal", "hafif", "evet");
+            tablo.Rows.Add("yağmurlu", "soğuk", "normal", "kuvvetli", "hayir");
+            tablo.Rows.Add("bulutlu", "soğuk", "normal", "kuvvetli", "evet");
+            tablo.Rows.Add("güneşli", "ılık", "yüksek", "hafif", "hayir");
+            tablo.Rows.Add("güneşli", "soğuk", "normal", "hafif", "evet");
+            tablo.Rows.Add("yağmurlu", "ılık", "normal", "hafif", "evet");
+            tablo.Rows.Add("güneşli", "ılık", "normal", "kuvvetli", "evet");
+            tablo.Rows.Add("bulutlu", "ılık", "yüksek", "kuvvetli", "evet");
+            tablo.Rows.Add("bulutlu", "sıcak", "normal", "hafif", "evet");
+            tablo.Rows.Add("yağmurlu", "ılık", "yüksek", "kuvvetli", "hayir");
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cmbHava.Text == "Güneşli" & cmbNem.Text == "Yüksek")
+            { txtSonuc.Text = "Hayır"; }
+
+            else if (cmbHava.Text == "Güneşli" & cmbNem.Text == "Normal")
+            { txtSonuc.Text = "Evet"; }
+
+            else if (cmbHava.Text == "Bulutlu")
+            { txtSonuc.Text = "Evet"; }
+
+            else if (cmbHava.Text == "Yağmurlu" & cmbRüzgar.Text == "Kuvvetli")
+            { txtSonuc.Text = "Hayır"; }
+
+            else if (cmbHava.Text == "Yağmurlu" & cmbRüzgar.Text == "Hafif")
+            { txtSonuc.Text = "Evet"; }
+
+
+
+        }
+
+        private void btnAgc_Click(object sender, EventArgs e)
+        {
+
+            g = pictureBox1.CreateGraphics();
+            
+            Point drawPoint1 = new Point(325, 20);
+            g.DrawString("Hava", drawFont, myBrush, drawPoint1);
+            g.DrawEllipse(pen2, 300, 20, 120, 25);
+            g.DrawLine(pen1, 350, 50, 350, 100);
+            g.DrawLine(pen1, 350, 50, 300, 100);
+            g.DrawLine(pen1, 350, 50, 435, 100);
+            Point drawPoint2 = new Point(235, 95);
+            Point drawPoint3 = new Point(325, 95);
+            Point drawPoint4 = new Point(410, 95);
+            g.DrawString("Güneşli", drawFont, myBrush, drawPoint2);
+            g.DrawString("Bulutlu", drawFont, myBrush, drawPoint3);
+            g.DrawString("Yağmurlu", drawFont, myBrush, drawPoint4);
+
+           g.DrawLine(pen1, 350, 115, 350, 145);
+           Point drawPoint6 = new Point(325, 150);
+
+            g.DrawString("Evet", drawFont, myBrush, drawPoint6);
+            g.DrawRectangle(pen2, 320, 142, 60, 40);
+            g.DrawLine(pen1, 285, 115, 210, 175);
+
+            Point drawPoint5 = new Point(185, 175);
+             g.DrawString("Nem", drawFont, myBrush, drawPoint5);
+             g.DrawEllipse(pen2, 155, 175, 120, 25);
+
+            g.DrawLine(pen1, 100, 300, 200, 200);
+            Point drawPoint7 = new Point(70, 290);
+            g.DrawString("Yüksek", drawFont, myBrush, drawPoint7);
+
+            g.DrawLine(pen1, 100, 310, 100, 400);
+            Point drawPoint9 = new Point(75, 410);
+            g.DrawRectangle(pen2, 75, 400, 60, 40);
+            g.DrawString("Hayır", drawFont, myBrush, drawPoint9);
+
+            g.DrawLine(pen1, 200, 200, 265, 285);
+            Point drawPoint8 = new Point(225, 290);
+            g.DrawString("Normal", drawFont, myBrush, drawPoint8);
+
+
+            g.DrawLine(pen1, 270, 310, 270, 405);
+            Point drawPoint10 = new Point(250, 410);
+            g.DrawRectangle(pen2, 250, 400, 60, 40);
+            g.DrawString("Evet", drawFont, myBrush, drawPoint10);
+
+            g.DrawLine(pen1, 440, 100, 500, 170);
+            Point drawPoint11 = new Point(460, 175);
+            g.DrawString("Rüzgarlı", drawFont, myBrush, drawPoint11);
+            g.DrawEllipse(pen2, 445, 175, 120, 25);
+
+            g.DrawLine(pen1, 500, 200, 450, 265);
+            Point drawPoint12 = new Point(400, 265);
+            g.DrawString("Kuvvetli", drawFont, myBrush, drawPoint12);
+
+            g.DrawLine(pen1, 500, 200, 550, 265);
+            Point drawPoint13 = new Point(525, 265);
+            g.DrawString("Hafif", drawFont, myBrush, drawPoint13);
+
+            g.DrawLine(pen1, 560, 285, 560, 400);
+            Point drawPoint14 = new Point(535, 405);
+            g.DrawRectangle(pen2, 535, 400, 60, 40);
+            g.DrawString("Evet", drawFont, myBrush, drawPoint14);
+
+            g.DrawLine(pen1, 435, 410, 435, 295);
+            Point drawPoint15 = new Point(400, 405);
+            g.DrawRectangle(pen2, 400, 400, 60, 40);
+            g.DrawString("Hayır", drawFont, myBrush, drawPoint15);
+
+
+        }
+
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+         
+           
+        }
+
+        private void btnEntropi_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int j = 4;
+            int evet = 0;
+            int hayir = 0;
+            double entropiO = 0;
+            double entropiI = 0;
+            double entropiR = 0;
+            double entropiN = 0;
+            double entropiH = 0;
+
+            double entropiO2 = 0;
+            double entropiI2 = 0;
+            double entropiN2 = 0;
+            
+
+
+
+            for (i = 0; i <= 13; i++)
+                if (Convert.ToString(dataGridView1.Rows[i].Cells[j].Value).StartsWith("e") == true)
+                {
+                    evet++;
+
+                }
+
+                else
+                {
+                    hayir++;
+
+                }
+
+            entropiO += -(Convert.ToDouble(hayir) / 14 * (Math.Log((Convert.ToDouble(hayir) / 14), 2)) + Convert.ToDouble(evet) / 14 * (Math.Log((Convert.ToDouble(evet) / 14), 2)));
+            listBox1.Items.Add("oyunun entropisi= " + entropiO.ToString());
+
+
+            entropiI = 0.2857142857 * (0.811)+ 0.4285714286*(0.918)+ 0.2857142857*(1.0);
+            entropiI = entropiO - entropiI;
+            listBox1.Items.Add("ısının entropisi= " + entropiI.ToString());
+
+
+            entropiR = 0.5714285714 * (0.811) + 0.4285714286 * (1.00) ;
+            entropiR = entropiO - entropiR;
+            listBox1.Items.Add("rüzgarın entropisi= " + entropiR.ToString());
+
+
+            entropiN = 0.5 * (0.985) + 0.5*(0.592);
+            entropiN = entropiO - entropiN;
+            listBox1.Items.Add("nemin entropisi= " + entropiN.ToString());
+
+
+            entropiH =  0.3571428571* (0.971) + 0.3571428571 * (0.971);
+            entropiH = entropiO - entropiH;
+            listBox1.Items.Add("havanın entropisi= " + entropiH.ToString());
+
+            listBox1.Items.Add("Entropinin 2. adımı");
+
+
+            entropiO2 = -(Convert.ToDouble(Math.Log((0.4), 2)*(0.4))+Convert.ToDouble(Math.Log((0.6),2)*(0.6)));
+            listBox1.Items.Add("oyunun entropisi= " + entropiO2.ToString());
+
+            entropiI2 = 0.4;
+            entropiI2 = entropiO2 - entropiI2;
+            listBox1.Items.Add("ısının entropisi= " + entropiI2.ToString());
+
+
+            entropiN2 = entropiO2 - entropiN2;
+            listBox1.Items.Add("nemin entropisi= " + entropiN2.ToString());
+
+            listBox1.Items.Add("rüzgarın entropisi= 0.019434666" );
+
+
+
+
+        }
+
+
+        
+    } 
+
+    } 
+
+
+
